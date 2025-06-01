b4fa8cd3b19841e04863ec3c74fd6100e2401239
hero0407 <mouryagoguri4@gmail.com>
2025-06-01 23:42:44 +0530

    Update week12.java

--- week12.java
+++ week12.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class exp12 {
 
 	public static void main(String[] args) throws InterruptedException, AWTException {
-		// TODO Auto-generated method stub
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.ilovepdf.com/word_to_pdf");
 		driver.manage().window().maximize();
